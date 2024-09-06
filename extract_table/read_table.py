3decc25989fd6c2410dfffaad2c7166b05032474
lachlan perrier <lachlanperrier@gmail.com>
2024-09-06 15:48:53 -0400

    mid edit

--- extract_table/read_table.py
+++ extract_table/read_table.py
@@ -34,12 +34,19 @@ def _stop_reading_table(line):
     return line == ""
 
 
-# TODO might replace with pandas methods read_fwt
 def read_table(line_iterator: iter) -> dict:
     """
     given iterator at current line of a file, will read the next table and return it as a
     dictionary.
     """
+
+    def parse_line(line, column_break_loc):
+        # +1 because we want to avoid the space between columns
+        # print(column_break_loc)
+        starts = [0] + list(column_break_loc)
+        ends = list(column_break_loc) + [len(line) + 99999]
+        return [line[start:end].strip() for start, end in zip(starts, ends)]
+
     return_dict = dict()
     column_names: list[str] = None
     column_break_locations: list[str] = None
@@ -57,29 +64,64 @@ def read_table(line_iterator: iter) -> dict:
         if column_names is not None:
             if _stop_reading_table(current_line):
                 return rows, column_names
-            rows.append(_parse_line(current_line, column_break_locations))
+            rows.append(parse_line(current_line, column_break_locations))
 
-        if set(current_line) == {"-", " "} or set(current_line) == {"=", " "}:
+        elif set(current_line) == {"-", " "} or set(current_line) == {"=", " "}:
             # May need to change .replace("  ", " ") in future
-            prev_column_break_locations = column_break_locations
             column_break_locations = np.where(
                 np.array(list(current_line.replace("  ", " "))) == " "
             )[0]
-
             # get column names
-            # this code only works on 2 levels
-            if column_names is None:
-                column_names = _parse_line(prev_line, column_break_locations)
-                previous_column_names = prev_unstriped_line
-                previous_column_break_footer = unstriped_current_line
+            column_names = parse_line(prev_line, column_break_locations)
 
-            else:
-                column_names = _parse_second_row(
-                    prev_line,
-                    column_break_locations,
-                    previous_column_names,
-                    previous_column_break_footer,
-                )
+
+# # # TODO might replace with pandas methods read_fwt
+# def read_table(line_iterator: iter) -> dict:
+#     """
+#     given iterator at current line of a file, will read the next table and return it as a
+#     dictionary.
+#     """
+#     return_dict = dict()
+#     column_names: list[str] = None
+#     column_break_locations: list[str] = None
+#     rows = []
+
+#     index, unstriped_current_line = next(line_iterator)
+#     current_line = unstriped_current_line.strip()
+#     while True:
+#         prev_line = current_line  # used for finding the header
+#         prev_unstriped_line = unstriped_current_line
+#         index, unstriped_current_line = next(line_iterator)
+#         current_line = unstriped_current_line.strip()
+#         # if column names is not none we have found table and are reading
+#         # Else, find a table
+#         if column_names is not None:
+#             if _stop_reading_table(current_line):
+#                 return rows, column_names
+#             rows.append(_parse_line(current_line, column_break_locations))
+
+#         if set(current_line) == {"-", " "} or set(current_line) == {"=", " "}:
+#             # May need to change .replace("  ", " ") in future
+#             prev_column_break_locations = column_break_locations
+#             column_break_locations = np.where(
+#                 np.array(list(current_line.replace("  ", " "))) == " "
+#             )[0]
+
+#             # get column names
+#             # this code only works on 2 levels
+#             if column_names is None:
+#                 column_names = _parse_line(prev_line, column_break_locations)
+#                 previous_column_names = prev_unstriped_line
+#                 previous_column_break_footer = unstriped_current_line
+
+#             else:
+#                 raise Exception("Table has double Index")
+#                 # column_names = _parse_second_row(
+#                 #     prev_line,
+#                 #     column_break_locations,
+#                 #     previous_column_names,
+#                 #     previous_column_break_footer,
+#                 # )
 
 
 def _find_indexes_col_name(
@@ -117,6 +159,18 @@ def _parse_second_row(
     #Trips     Miles        Hours    #Trips     Miles        Hours    #Trips     Miles        Hours
     ====== ============ ============ ====== ============ ============ ====== ============ ============
 
+    we will return something like this:
+    [
+        (Y2023 EXISTING, #Trips),
+        (Y2023 EXISTING, Miles),
+        (Y2023 EXISTING, Hours),
+        (Y2023 NO-BUILD, #Trips),
+        (Y2023 NO-BUILD, Miles),
+        (Y2023 NO-BUILD, Hours),
+        (Y2023 BUILD, #Trips),
+        (Y2023 BUILD, Miles),
+        (Y2023 BUILD, Hours),
+    ]
     thinking: when we are reading column sub heading names
     we go up 1 level
     we go left until we see a space
@@ -135,12 +189,12 @@ def _parse_second_row(
         col_start, col_end = _find_indexes_col_name(
             previous_column_break_footer, sub_col_start
         )
-        print(col_start, col_end)
         col_name = previous_column_names[col_start:col_end].strip()
 
         return_list.append(col_name + "_" + sub_col_name)
 
     # [line[start:end].strip() for start, end in zip(starts, ends)]
+    print(return_list)
     return return_list
 
 
@@ -189,6 +243,7 @@ def get_table(path: Union[str, Path], table_number: str):
 def main(config: dict):
     input_dir = Path(config["input_dir"])
     output_dir = Path(config["output_dir"])
+    ret_dfs = []
 
     for prn_file_path in input_dir.glob("*.prn"):
         name = prn_file_path.stem
@@ -205,6 +260,9 @@ def main(config: dict):
 
             formatted_name = f"{split_table[0]} {split_table[-1]}"
             df.to_csv(output_dir / name / f"{formatted_name}.csv")
+            ret_dfs.append(df)
+
+    return ret_dfs
 
 
 if __name__ == "__main__":
@@ -212,7 +270,7 @@ if __name__ == "__main__":
     with open(cwd / "config.yml") as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    main(config)
+    dfs = main(config)
 
 
 # %%
